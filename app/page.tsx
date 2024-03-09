b1dd1c90d938922a13c65896d41d52469dfb3815
Brian Liebe <brianjliebe@gmail.com>
2024-03-09 13:14:12 -0500

    add separate skills tab

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,15 @@ import {
   Education,
   Home,
   Launch,
+  ToolKit
 } from "@carbon/icons-react";
 
 export default function HomePage() {
   const iconHome = Home as ComponentType<{ size: number }>;
   const iconWork = BuildingInsights_2 as ComponentType<{ size: number }>;
-  const iconProjects = DataCategorical as ComponentType<{ size: number }>;
   const iconEducation = Education as ComponentType<{ size: number }>;
+  const iconSkills = ToolKit as ComponentType<{ size: number }>;
+  const iconProjects = DataCategorical as ComponentType<{ size: number }>;
   const iconContact = At as ComponentType<{ size: number }>;
 
   return (
@@ -29,6 +31,9 @@ export default function HomePage() {
           </Tab>
           <Tab renderIcon={iconEducation} style={{ marginRight: "3px" }}>
             Education
+          </Tab>
+          <Tab renderIcon={iconSkills} style={{ marginRight: "3px" }}>
+            Skills
           </Tab>
           <Tab renderIcon={iconProjects} style={{ marginRight: "3px" }}>
             Projects
@@ -111,22 +116,49 @@ export default function HomePage() {
             </p>
           </TabPanel>
           <TabPanel>
-            <h3>Languages</h3>
+            <h3>Front-end</h3>
             <p>
-              C# / .NET
+              React.js
               <br />
-              JavaScript • TypeScript • React.js • Vue.js
+              Vue.js
+              <br />
+              JavaScript
+              <br />
+              TypeScript
+            </p>
+            <h3>Back-end</h3>
+            <p>
+              C# / .NET / ASP.NET
               <br />
               Python
               <br />
-              SQL • MySql • MongoDB
+              SQL
+              <br />
+              MySQL
+              <br />
+              MongoDB
               <br />
               Java
               <br />
+              TCL
             </p>
-            <h3>Wordle Discord Bot</h3>
+            <h3>Additional</h3>
             <p>
-              Discord chat bot that detects and tracks Wordle scores
+              Git / GitHub / Actions
+              <br />
+              Microsoft Office / VSTO
+              <br />
+              DataDog
+              <br />
+              Splunk
+              <br />
+              OpenCV
+            </p>
+          </TabPanel>
+          <TabPanel>
+            <h3>NYT Games Discord Bot</h3>
+            <p>
+              Discord chat bot that detects and tracks Wordle, Connections, and Strands scores
               <br />
               <small>
                 <i>Python • MySQL</i>
@@ -134,7 +166,7 @@ export default function HomePage() {
               <br />
               <small>
                 <a
-                  href="https://github.com/brianliebe/wordle-bot"
+                  href="https://github.com/brianliebe/nyt-games-bot"
                   target="_blank"
                 >
                   View on GitHub
